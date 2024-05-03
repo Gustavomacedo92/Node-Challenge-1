11ab1298abc9bd208a0c0f193d65f97fc968e8e0
Gustavomacedo92 <gustavo.mf@outlook.com>
2024-05-03 11:02:49 -0300

    projeto add usuarios BackEnd

--- index.js
+++ index.js
@@ -1,107 +1,84 @@
+
+
+const cors = require('cors');
+
+
+
 const express = require("express")
 const uuid = require('uuid')
 const port = 3001
 const app = express()
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 
-const orders = []
 
-const Middlewares = (request, response, next) => {
+const idCheck = (request, response, next) => {
     const { id } = request.params
-
-    const index = orders.findIndex(client => client.id === id)
+    const index = users.findIndex(user => user.id === id)
 
     if (index < 0) {
-        return response.status(404).json({ error: "user not fond" })
 
+        return response.status(404).json({ error: "user not fund" })
     }
 
     request.userIndex = index
     request.userId = id
-    request.order = orders[index]
-    next()
-}
 
-const Middlewares2 = (request, response, next) => {
-   const method = request.method
-   const url = request.url
 
-   console.log(`method: ${method} url: ${url}`)
-    
     next()
 }
 
 
+const users = []
 
-app.get("/orders", Middlewares2,  (request, response) => {
-    return response.json(orders)
+app.get("/users", (request, response) => {
+
+    return response.json(users)
+
 })
 
 
+app.post("/users",  (request, response) => {
 
-app.post("/orders",Middlewares2, (request, response) => {
-    const { order, clientName, price } = request.body
-    const status = "em preparação"
+    const { name, age } = request.body
 
-    const pedido = { id: uuid.v4(), order, clientName, price, status }
+    const order = { id: uuid.v4(), name, age }
 
-    orders.push(pedido)
+    users.push(order)
 
-    return response.json(pedido)
+    return response.status(201).json(order)
+
 })
 
 
+app.put("/users/:id",idCheck, (request, response) => {
 
-app.put("/orders/:id", Middlewares,Middlewares2, (request, response) => {
-
-    const { order, clientName, price } = request.body
+    const { name, age } = request.body
     const index = request.userIndex
     const id = request.userId
+    const updateUser = { id, name, age }
 
-    const updateUser = { id, order, clientName, price }
-
-
-
-    orders[index] = updateUser
-    return response.json(orders)
-
-})
-
-app.delete("/orders/:id", Middlewares,Middlewares2, (request, response) => {
-
-    const { order, clientName, price } = request.body
-    const index = request.userIndex
-    const id = request.userId
-
-    const updateUser = { id, order, clientName, price }
-
-    orders.splice(index, 1)
-
-    return response.status(204).json()
-})
-
-
-app.get("/orders/:id", Middlewares,Middlewares2, (request, response) => {
-    const index = request.userIndex
-
-    return response.status(202).json(orders[index])
-})
-
-
-
-app.patch("/orders/:id", Middlewares,Middlewares2, (request, response) => {
-    const { order, clientName, price, } = request.order
-    const index = request.userIndex
-    const id = request.userId
-
-    const updateUser = { id, order, clientName, price, status: "pronto" }
-
-    orders[index] = updateUser
-
+    users[index] = updateUser
     return response.json(updateUser)
 
 })
+
+
+app.delete("/users/:id", idCheck,(request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+
+
+    users.splice(index, 1)
+
+
+
+    return response.status(204).json()
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`🏍 server started on port ${port} `)
